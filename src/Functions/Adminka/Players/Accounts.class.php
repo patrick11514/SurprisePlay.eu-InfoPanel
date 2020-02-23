19cd48dfe912298af5730ab13dd1c9cba781d721
patrick11514 <ja@patrick115.eu>
2020-02-23 01:30:52 +0100

    Přidáno zapisování do databáze Adminka

--- src/Functions/Adminka/Players/Accounts.class.php
+++ src/Functions/Adminka/Players/Accounts.class.php
@@ -2,8 +2,10 @@
 
 namespace patrick115\Adminka\Players;
 
-use patrick115\Main\Session;
 use patrick115\Main\Config;
+use patrick115\Main\Database;
+use patrick115\Main\Session;
+use patrick115\Main\Tools\Utils;
 
 class Accounts
 {
@@ -18,17 +20,21 @@ class Accounts
      */
     private $session;
 
+    private $database;
+
     public function __construct()
     {
-        $this->config = Config::init();
-        $this->session = Session::init();
+        $this->config   = Config::init();
+        $this->session  = Session::init();
+        $this->database = Database::init();
     }
 
     /**
      * Run account verification
      */
     public function run()
-    {   
+    {
+
         if (!$this->checkLoginStatus()) {
             $router = \patrick115\Adminka\Main::getApp("\patrick115\Main\Router");
             if (!$router->compare_pages("login")) {
@@ -42,17 +48,20 @@ class Accounts
                         if (\patrick115\Main\Tools\Utils::newNull($this->session->getData("Request/Data/password"))) {
                             unset($_SESSION["Post"]);
                             $_SESSION["Request"]["Errors"] = [
-                                "Toto jméno neexistuje"
+                                "Toto jméno neexistuje",
                             ];
                             \patrick115\Main\Tools\Utils::header("./?login");
                         } else if (!$this->session->getData("Request/Data/password")) {
                             unset($_SESSION["Post"]);
                             $_SESSION["Request"]["Errors"] = [
-                                "Zadal jsi nesprávné heslo"
+                                "Zadal jsi nesprávné heslo",
                             ];
                             \patrick115\Main\Tools\Utils::header("./?login");
                         } else {
-                            $this->loginUser($this->session->getData("Request/Data/name"), $this->session->getData("Request/Data/primary_group"));
+                            
+                            $this->loginUser($this->session->getData("Request/Data/name"), $this->session->getData("Request/Data/primary_group"), \patrick115\Main\Tools\Utils::getPackage(
+                                $this->session->getdata("Request/FromPost/password")
+                            ));
                         }
                     }
                 }
@@ -77,9 +86,12 @@ class Accounts
      * Login user
      * @param string $username
      * @param string $group
+     * @param string $lenght - passlength
      */
-    private function loginUser($username, $group)
+    private function loginUser($username, $group, $length)
     {
+
+        echo $length;
 
         session_destroy();
         session_start();
@@ -91,11 +103,30 @@ class Accounts
         } else {
             $_SESSION["Account"]["Admin_Account"] = false;
         }
-        $app = \patrick115\Adminka\Main::Create("\patrick115\Minecraft\Skins", [$username]);
-        $_SESSION["Account"]["User"]["Skin"] = $app->getSkin();
-        $_SESSION["Account"]["User"]["Logged"] = true;
+        $app                                     = \patrick115\Adminka\Main::Create("\patrick115\Minecraft\Skins", [$username]);
+        $_SESSION["Account"]["User"]["Skin"]     = $app->getSkin();
+        $_SESSION["Account"]["User"]["Logged"]   = true;
         $_SESSION["Account"]["User"]["Username"] = $username;
-        $_SESSION["Account"]["User"]["Group"] = $group;
+        $_SESSION["Account"]["User"]["Group"]    = $group;
+
+        $rv      = $this->database->execute("SELECT `id` FROM `main_authme`.`authme` WHERE `realname` = '{$username}'", true);
+        $user_id = $rv->fetch_object()->id;
+
+        $rv = $this->database->execute("SELECT `id` FROM `accounts` WHERE `authme_id` = " . $user_id, true);
+        if ($this->database->num_rows($rv) == 0) {
+            $this->database->insert("accounts", ["id", "authme_id", "e-mail", "last-ip", "ip-list"], ["", $user_id, "", Utils::getUserIP(), "{}"]);
+        }
+
+        $cid = $this->database->select(["id"], "accounts", "LIMIT 1", "authme_id", $user_id)->
+            fetch_object()->id;
+        $rv = $this->database->select(["pass_length"], "pass_storage", "LIMIT 1", "user_id", $cid);
+        if ($this->database->num_rows($rv) == 0) {
+            $this->database->insert("pass_storage", ["id", "user_id", "pass_length"], ["", $cid, mb_strlen($length)]);
+        } else {
+            if (mb_strlen($length) != $rv->fetch_object()->pass_length) {
+                $this->database->update("pass_storage", "user_id", $cid, ["pass_length"], [mb_strlen($length)]);
+            }
+        }
 
         session_write_close();
 
