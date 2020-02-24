47f0d5aaafba606e4523dd1b0a4d9800fca640ba
patrick11514 <ja@patrick115.eu>
2020-02-24 23:37:42 +0100

    Develop mode

--- requests.php
+++ requests.php
@@ -9,11 +9,16 @@ if (empty($_POST)) {
     die("Invalid Request!");
 }
 
+print_r($_POST);
+
+
 Main::Create("\patrick115\Adminka\Main", [__DIR__]);
 
 $app = Main::Create("\patrick115\Requests\Core", [$_POST]);
 
 $app->check();
+
+print_r($app->getErrors());
 
 if (empty($app->getErrors())) {
     $_SESSION["Post"]["Success"] = true;
