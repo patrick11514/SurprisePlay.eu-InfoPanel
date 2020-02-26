96e25e74e96d20e11502c4620912e4da3ee48a40
patrick11514 <ja@patrick115.eu>
2020-02-27 00:20:11 +0100

    Odebrány debug pomůcky

--- src/Functions/Main/Database.class.php
+++ src/Functions/Main/Database.class.php
@@ -161,8 +161,6 @@ class Database extends Error
             $command = "SELECT $list FROM `$table` WHERE `$haystack` = '$needle' $options";
         }
 
-        echo $command . PHP_EOL;
-
         try {
             $return = $this->conn->query($command);
         } catch (Exception $e) {
@@ -181,7 +179,6 @@ class Database extends Error
      */
     public function execute($sql, $return = false)
     {
-        echo $sql . PHP_EOL;
 
         $rv = $this->conn->query(\patrick115\Main\Tools\Utils::chars($this->removeChars($sql)));
 
