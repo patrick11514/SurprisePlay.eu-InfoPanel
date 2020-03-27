fa7761608ec18c0119dee401fccada61157355d5
patrick11514 <ja@patrick115.eu>
2020-03-27 21:51:54 +0100

    Přidáno ověřování, zda jsou nainstalovány požadované addony

--- src/Functions/autoload.php
+++ src/Functions/autoload.php
@@ -45,6 +45,18 @@ if ($__DEBUG === true) {
     define("DEBUG", NULL);
 }
 
+$exts = [
+    "mbstring"
+];
+
+$ext_errs = [];
+
+foreach ($exts as $ext) {
+    if (!extension_loaded($ext)) {
+        $ext_errs[] = $ext;
+    }
+}
+
 $_616e6f6e796d6f7573 = function($_646174616261736573, $_64625f696e7374616e6365, $_6572726f725f66756e6374696f6e) {
     $error = [];
     foreach ($_646174616261736573 as $_6461746162617365)
@@ -61,8 +73,6 @@ $_616e6f6e796d6f7573 = function($_646174616261736573, $_64625f696e7374616e6365, 
             $_6572726f725f66756e6374696f6e->catchError(str_replace("{@var}", $err, \patrick115\Main\Tools\Utils::getPackage([1 => "4461746162617365207b407661727d20646f65736e27742065786973747321"])), debug_backtrace());
         }
     }
-
-
 };
 
 Constanter::init()->send("_616e6f6e796d6f7573", $_616e6f6e796d6f7573);
@@ -70,3 +80,9 @@ Constanter::init()->send("_616e6f6e796d6f7573", $_616e6f6e796d6f7573);
 Session::init()->create();
 
 $errors = Error::init();
+
+if (!empty($ext_errs)) {
+    foreach ($ext_errs as $ext_err) {
+        $errors->catchError("Extension {$ext_err} is not loaded!", debug_backtrace());
+    }
+}
