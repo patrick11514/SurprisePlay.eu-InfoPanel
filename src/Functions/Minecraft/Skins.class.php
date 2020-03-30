151a64d559f572720de5f9213a36e897993c3e17
patrick11514 <ja@patrick115.eu>
2020-03-30 02:52:18 +0200

    Přidáno cashování hashů skinů pro rychlejší načítání

--- src/Functions/Minecraft/Skins.class.php
+++ src/Functions/Minecraft/Skins.class.php
@@ -2,20 +2,67 @@
 
 namespace patrick115\Minecraft;
 
+use patrick115\Adminka\Main;
+use patrick115\Main\Tools\Utils;
+use patrick115\Adminka\Logger;
+
 class Skins
 {
     private $username;
 
-    private $api = "https://minotar.net/bust/{username}/50";
+    private $logger;
+
+    private $api = "https://visage.surgeplay.com/bust/96/{uuid}";
 
     public function __construct($username)
     {
         $this->username = $username;
+        $this->logger = Logger::init();
     }
 
     public function getSkin()
     {
-        $skin_data = file_get_contents(str_replace("{username}", $this->username, $this->api));
-        return "data:image/png;base64," . base64_encode($skin_data);
+        if ($this->inCache()) {
+            return $this->getCache();
+        }
+
+        return $this->createCache();
     }
+
+    private function inCache()
+    {
+        if (!is_writable(Main::getWorkDirectory() . "src/cache")) {
+            $this->logger->log("Cache directory doesn't have permissions, skipping cashing!", "warning", true);
+            return false;
+        }
+        if (file_exists(Main::getWorkDirectory() . "src/cache/{$this->username}"))
+        {
+            return true;
+        }
+        return false;
+    }
+
+    private function getCache()
+    {
+        return file_get_contents(Main::getWorkDirectory() . "src/cache/{$this->username}");
+    }
+
+    private function createCache()
+    {
+        $uuid = Utils::getOriginalUUIDByNick($this->username);
+
+        $skin_data = file_get_contents(str_replace("{uuid}", $uuid, $this->api));
+
+        $base64 = "data:image/png;base64," . base64_encode($skin_data);
+        if (!is_writable(Main::getWorkDirectory() . "src/cache")) {
+            $this->logger->log("Cache directory doesn't have permissions, skipping cashing!", "warning", true);
+            return $base64;
+        }
+        $f = fopen(Main::getWorkDirectory() . "src/cache/{$this->username}", "w");
+        fwrite($f, $base64);
+        fclose($f);
+
+        return $base64;
+    }
+    //https://visage.surgeplay.com/bust/96/f018d5d53fa6431ab687ea80cf7e1a14
 }
