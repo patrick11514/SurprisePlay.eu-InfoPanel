06674bc56d0f5d4a08b4c9ddd1bcffbea1c74542
patrick11514 <ja@patrick115.eu>
2020-03-30 02:52:40 +0200

    Odebrán define na PHP_VERSION

--- src/Functions/autoload.php
+++ src/Functions/autoload.php
@@ -7,8 +7,6 @@ use patrick115\Main\Tools\Constanter;
 
 define("CURRENT_VERSION", "0.2.8");
 
-define("PHP_VERSION", PHP_VERSION);
-
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
