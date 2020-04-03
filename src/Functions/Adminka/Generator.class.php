39838af899ede61bffedbfb421b87962acb8eb0c
patrick11514 <ja@patrick115.eu>
2020-04-03 20:28:56 +0200

    Opravena dvojtecka

--- src/Functions/Adminka/Generator.class.php
+++ src/Functions/Adminka/Generator.class.php
@@ -98,7 +98,8 @@ class Generator
             "Unregister",
             "Gems",
             "Todo",
-            "Change-User-Data"
+            "Change-User-Data",
+            "Unban"
         ];
 
         if (!in_array($form_name, $stored_forms)) {
@@ -587,6 +588,51 @@ class Generator
                     </div>
                 </div>
             ';
+            break;
+            case "Unban":
+                $this->genData = '
+                <div id="content">
+                    <nav class="navbar navbar-expand-lg navbar-light">
+                        <div class="container-fluid">
+                            <ul class="nav navbar-nav">
+                                <li class="nav-item">
+                                    <a class="nav-link" id="sidebarCollapse" href="#"><i class="fas fa-align-left"></i></a>
+                                </li>
+                            </ul>
+                            <p>%%page_name%%</p>
+                            <ul class="nav navbar-nav ml-auto">
+                                <li class="nav-item">
+                                    <a class="nav-link" href="?logout"><i class="fas fa-sign-in-alt"></i></a>
+                                </li>
+                            </ul>
+                        </div>
+                    </nav>
+
+                    <div id="container" class="container-fluid">
+                        %%ERRORS%%
+                        %%messages%%
+                        <div class="card">
+                            <div class="card-body">
+                                <p>Odbanování uživatele</p>
+                            	<hr>
+                                <form action="./requests.php" method="post">
+                                    <input type="hidden" name="method" value="unban" required>
+                                    <input type="hidden" name="source_page" value="?unban" required>
+                                    <input type="hidden" name="CSRF_token" id="CSRF_TOKEN" value="%%CSRF_Token%%" required>
+                                    <div class="form-group">
+                                        <label for="nick">Nick</label>
+                                        <input type="text" id="nick" name="nick" class="form-control" required>
+                                        <div class="list-group" id="nicks">
+                                        </div>
+                                    </div>
+
+                                    <button type="submit" id="confirm-button" class="btn btn-light">Odbanovat</button>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                ';
             break;
             default:
                 $this->genData = '
