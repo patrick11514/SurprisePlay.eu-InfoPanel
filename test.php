ad2e23da1bf2d56163156e7d9c5fbeb8ca070009
patrick11514 <ja@patrick115.eu>
2020-04-03 15:49:57 +0200

    Smazán testovací soubor

--- test.php
+++
@@ -1,6 +0,0 @@
-<form method="post" action="API.php">
-    <input type="text" placeholder="Value" name="page">
-    <input type="text" placeholder="Token" name="CSRF_TOKEN">
-    <input type="text" placeholder="Metoda" name="method">
-    <button>Poslat na API</button>
-</form>
