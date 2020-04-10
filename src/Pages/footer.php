b1ae63647720961cfa564d1c06c866e35c8f22a4
Skypad6000 <60575209+Skypad6000@users.noreply.github.com>
2020-04-10 03:17:38 +0200

    Footer

---
+++ src/Pages/footer.php
@@ -0,0 +1,15 @@
+<div id="footer">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-4 text-left">
+                Verze: 0.4.2
+            </div>
+            <div class="col-md-4 text-center">
+                &copy; 2019 - 2020 SurprisePlay.eu
+            </div>
+            <div class="col-md-4 text-right">
+                <a href=?>ChangeLog</a>
+            </div>
+        </div>
+    </div>
+</div>
